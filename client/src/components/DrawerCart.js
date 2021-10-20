dad60ade57a071b97b854333264da287adfd87d9
mishrakeshav <47028747+mishrakeshav@users.noreply.github.com>
2021-10-20 13:59:00 +0530

    add cart drawer

---
+++ client/src/components/DrawerCart.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Icon } from '@iconify/react';
+import { Form, FormikProvider } from 'formik';
+import closeFill from '@iconify/icons-eva/close-fill';
+import roundClearAll from '@iconify/icons-ic/round-clear-all';
+import roundFilterList from '@iconify/icons-ic/round-filter-list';
+// material
+import {
+
+  Button,
+  Drawer,
+
+} from '@mui/material';
+
+//
+
+import Cart from '../pages/Cart';
+import { ProductCartWidget } from './_dashboard/products';
+
+// ----------------------------------------------------------------------
+
+export const SORT_BY_OPTIONS = [
+  { value: 'featured', label: 'Featured' },
+  { value: 'newest', label: 'Newest' },
+  { value: 'priceDesc', label: 'Price: High-Low' },
+  { value: 'priceAsc', label: 'Price: Low-High' }
+];
+export const FILTER_GENDER_OPTIONS = ['Men', 'Women', 'Kids'];
+export const FILTER_CATEGORY_OPTIONS = ['All', 'Shose', 'Apparel', 'Accessories'];
+export const FILTER_RATING_OPTIONS = ['up4Star', 'up3Star', 'up2Star', 'up1Star'];
+export const FILTER_PRICE_OPTIONS = [
+  { value: 'below', label: 'Below $25' },
+  { value: 'between', label: 'Between $25 - $75' },
+  { value: 'above', label: 'Above $75' }
+];
+export const FILTER_COLOR_OPTIONS = [
+  '#00AB55',
+  '#000000',
+  '#FFFFFF',
+  '#FFC0CB',
+  '#FF4842',
+  '#1890FF',
+  '#94D82D',
+  '#FFC107'
+];
+
+// ----------------------------------------------------------------------
+
+DrawerCart.propTypes = {
+  isOpenFilter: PropTypes.bool,
+  onResetFilter: PropTypes.func,
+  onOpenFilter: PropTypes.func,
+  onCloseFilter: PropTypes.func,
+  formik: PropTypes.object
+};
+
+export default function DrawerCart({
+  isOpenFilter,
+  onOpenFilter,
+  onCloseFilter,
+}) {
+  
+  
+  return (
+    <>
+      <ProductCartWidget onClick={onOpenFilter}></ProductCartWidget>
+          <Drawer
+            anchor="right"
+            open={isOpenFilter}
+            onClose={onCloseFilter}
+            PaperProps={{
+              sx: { width: '80%', border: 'none', overflow: 'hidden' }
+            }}
+          >
+            <Cart/>
+          </Drawer>
+    </>
+  );
+}
