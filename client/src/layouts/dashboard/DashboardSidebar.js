5584dc8c3378170cc682cede4f88f4aca7fac2d3
mishrakeshav <47028747+mishrakeshav@users.noreply.github.com>
2021-10-20 18:16:48 +0530

    user crud done

--- client/src/layouts/dashboard/DashboardSidebar.js
+++ client/src/layouts/dashboard/DashboardSidebar.js
@@ -69,7 +69,7 @@ export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
             <Avatar src={account.photoURL} alt="photoURL" />
             <Box sx={{ ml: 2 }}>
               <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
-                {account.displayName}
+                {'Keshav Mishra'}
               </Typography>
               <Typography variant="body2" sx={{ color: 'text.secondary' }}>
                 {account.role}
